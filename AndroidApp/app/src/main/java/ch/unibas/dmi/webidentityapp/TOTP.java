21117f00520edfaaaaad504dd8d10625945513a9
phoeft670 <p.hoeft@stud.unibas.ch>
2018-12-03 12:40:57 +0100

    added truncation and slicing

--- AndroidApp/app/src/main/java/ch/unibas/dmi/webidentityapp/TOTP.java
+++ AndroidApp/app/src/main/java/ch/unibas/dmi/webidentityapp/TOTP.java
@@ -1,6 +1,7 @@
 package ch.unibas.dmi.webidentityapp;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyStore;
@@ -12,6 +13,7 @@ import java.security.Provider;
 import java.security.Security;
 import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -25,17 +27,25 @@ public class TOTP {
     ks.load(null);
     this.alias = alias;
   }
-  public byte[] getKey(byte[] counter,String alias)
+
+  public int getKey(byte[] counter,String alias)
       throws NoSuchAlgorithmException, UnrecoverableEntryException, KeyStoreException, InvalidKeyException {
     Mac mac = Mac.getInstance("HmacSha1");
     KeyStore.Entry key = ks.getEntry(alias,null);
     if (!(key instanceof Key)) {
       System.err.println("Key Store Entry with alias: "+alias+"wasn't a key");
-      return null;
+      return -1;
     }
     mac.init((Key)key);
     mac.update(counter);
-    return mac.doFinal();
+    byte[] result =  mac.doFinal();
+    byte offset = result[result.length-1];
+    offset &= 15;
+    byte[] trunc =  Arrays.copyOfRange(result,offset,offset+3);
+    trunc[0]&=127;
+    int truncint = ByteBuffer.wrap(trunc).getInt();
+    truncint %= 1000000;
+    return truncint;
   }
 
 }
