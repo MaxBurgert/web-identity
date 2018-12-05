a51d9f3ac8970a115bebdb80cb2a08bf494dca68
timko98 <tim.koenigl@unibas.ch>
2018-12-05 14:01:55 +0100

    Created iterator over the meta data of the request to get shib info

--- Django/ShibService/authorizer/views.py
+++ Django/ShibService/authorizer/views.py
@@ -1,8 +1,22 @@
+import os
+from pprint import pprint
+from django.conf import settings
+
 from django.shortcuts import render
 
 
 # Create your views here.
 
 def index(request):
-    print(request)
+    for key in request.META:
+        print(str(key))
+        print(str(request.META[key]))
+
+    if os.access(__file__, os.R_OK):
+        print('Can Read')
+    if os.access(__file__, os.W_OK):
+        print('Can Write')
+    if os.access(__file__, os.X_OK):
+        print('Can execute')
+
     return render(request, 'auth-index.html')
